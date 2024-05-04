2c3c2120601eeda5561b3a800d65cd51f591ed03
Yeenke <125749946+Yogesh9389@users.noreply.github.com>
2024-05-04 04:38:49 +0000

    datatypes

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,20 @@
+//  Data types
+
+// Premitive Data-type:
+
+
+// number => Represents both integer and floating-point numbers. (2 to power 53)
+// bigint => used to store very large numbers.
+//  string => Represents a sequence of characters, enclosed in single or double quotes.("Yogesh")
+//  boolen => true / false
+// null => standalone value.
+//  undefined => Represents a variable that has been declared but not assigned a value.
+//  symbol => Represents a unique identifier.
+
+// Non-primitive Data Type:
+
+// Object: Represents a collection of key-value pairs, where keys are strings (or Symbols) and values can be of any data type, including other objects.
+
+console.log(typeof "Yogesh"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
